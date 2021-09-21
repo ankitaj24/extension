03d5fc6cf44ed0df7e1cc6f6235b91080ca051c7
ankitaj24 <86417055+ankitaj24@users.noreply.github.com>
2021-09-21 22:28:26 +0530

    Update init.js

--- init.js
+++ init.js
@@ -4,12 +4,9 @@
         var img=document.createElement("img");
         img.src='https://img.icons8.com/material-rounded/24/000000/repeat.png';
         btn.value="LOOP";
-        img.style.height="10";
         btn.style="border: none";
         btn.style.backgroundColor="#f9f9f9";
         btn.style.fontWeight='bold';
-        btn.id="hhhh";
-        btn.type="submit";
         document.querySelector("ytd-menu-renderer.style-scope.ytd-video-primary-info-renderer #top-level-buttons-computed").appendChild(img);
         document.querySelector("ytd-menu-renderer.style-scope.ytd-video-primary-info-renderer #top-level-buttons-computed").appendChild(btn);
     }
